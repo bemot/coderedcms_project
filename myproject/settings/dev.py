9282cca8be1e90175a91726e5a6eb6aad6b8cf52
sasha <sasha@debianmain>
2020-12-04 23:15:01 +0200

    first commit to git

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'hmnbx1+(xx21usr49^kw4f56s3)ov#ld_ne^6x79s4wl0z9(2x'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
